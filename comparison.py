5111f651a01c9a26409504465e11f60337ca7f89
Janice Cotcher <janicemcotcher@gmail.com>
2026-05-02 21:43:31 -0600

    update

--- comparison.py
+++ comparison.py
@@ -4,35 +4,55 @@ from spherical_utilities import *
 from new import data
 import matplotlib.pyplot as plt
 # import plotly
-cut = np.array(data)
-triangles = []
-points = []
-x = []
-y = []
-z = []
-for triangle in data:
-    triangles.append(triangle)
-    for point in triangle:
-        points.append(point)
-        x.append(point[0])
-        y.append(point[1])
-        z.append(point[2])
+# import trimesh
+# from scipy.spatial import Delaunay
 
-r,theta,phi = cartesian_to_spherical(np.array(x),np.array(y),np.array(z))
-new_phi = phi + np.deg2rad(45)
-new_x, new_y, new_z = spherical_to_cartesian(r, theta, new_phi)
-old_mesh = np.stack([x,y,z], axis=-1)
-new_mesh = np.stack([new_x, new_y, new_z], axis=-1)
+triangles = np.array(data)
+
+points = [(point for point in triangle) for triangle in triangles]
+print(points)
+# x = []
+# y = []
+# z = []
+# for triangle in data:
+#     triangles.append(triangle)
+#     for point in triangle:
+#         points.append(point)
+#         x.append(point[0])
+#         y.append(point[1])
+#         z.append(point[2])
+# x = np.array(x)
+# y = np.array(y)
+# z = np.array(z)
+# r,theta,phi = cartesian_to_spherical(x,y,z)
+# new_phi = phi + np.deg2rad(45)
+# new_x, new_y, new_z = spherical_to_cartesian(r, theta, new_phi)
+# old_mesh = np.stack([x,y,z], axis=-1)
+# new_mesh = np.stack([new_x, new_y, new_z], axis=-1)
 
 
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.plot_surface(x, y, z)
-ax.set_aspect('equal')
+# X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
+# New_x, New_y, New_z = np.meshgrid(new_x, new_y, new_z)
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+# ax.plot_surface(x, y, z)
+# ax.set_aspect('equal')
 
 # plt.show()
-# ax2 = fig.add_subplot(111, projection='3d')
-# ax2.plot(new_x, new_y, new_z)
+# # ax2 = fig.add_subplot(111, projection='3d')
+# # ax2.plot(new_x, new_y, new_z)
 
-plt.savefig("comparison.png")
+# plt.savefig("comparison.png")
+
+# Flatten data for processing
+# points = np.vstack((x.flatten(), y.flatten(), z.flatten())).T
+
+# # 2. Triangulate the 2D grid points (X, Y)
+# points2d = np.vstack((x.flatten(), y.flatten())).T
+# tri = Delaunay(points2d)
+
+# # 3. Create the mesh
+# mesh = trimesh.Trimesh(vertices=points, faces=tri.simplices)
+
+# # 4. Export to STL
+# mesh.export("surface.stl")
